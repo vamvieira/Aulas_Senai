99946ef0a6dfd2a3dbb89781933ce2ae245a2561
Vitor Magalhães Vieira <71799009+vamvieira@users.noreply.github.com>
2025-05-29 17:13:31 -0300

    Fundamentos Python 1
    Aula inicial a programação Python e a conquista da certificação.

---
+++ inicial_operadores.py
@@ -0,0 +1,15 @@
+print("Soma:", 2+7)
+print("Subtração:", 9-6)
+print("Divisão:", 10/3)
+print("Multiplicação:",2*8)
+print("Divisão Inteira:", 10//3)
+print("Restor de Divisão:", 21%2)
+print("Expoente/Potência:", 5**2)
+
+#Desafios
+print("A area do triângulo é:", 7.89*8.74)
+print("A temperatura está aproximadamente:", 18*9/5, "\bFº")
+print("A área do seu cículo é:\n", 3.1415*1.5**2)
+
+
+
---
+++ inicial_print.py
@@ -0,0 +1,21 @@
+#Introdução ao print
+print("Welcome, Vitor!")
+
+#Uso da , para separção de textos
+print("Meu nome", "é Vitor")
+
+#Usando aspas simples e duplas para criação de sitação no texto
+print("Aula de 'Python'")
+print('he\'s')
+print("he's")
+
+#Usando o separador para substituir o espaço entre letras: sep
+print("Meu", "nome", "é", "Vitor", sep='+', end='=')
+
+#Usando o end para puxar a linha de texto anterior com a nova: end
+print("Minha idade é 25 anos")
+
+#Desafio
+#Printar Programação***Essenciais***em...Python, usando sep & end
+print("Programação", "Essenciais", "em", sep='***', end='...')
+print("Python")
